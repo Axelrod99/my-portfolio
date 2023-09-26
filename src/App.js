2c6711dfd00fc5ed7ee3f78a5ce7520556fa4f31
jibola <ajibolaajiboye99@gmail.com>
2023-09-26 14:04:24 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ import housepadi from "./assets/images/Housepadi - Google Chrome 9_26_2023 11_06
 import lamore from "./assets/images/lamore.b695576b18c22700a54e.png";
 import comptware from "./assets/images/Bani - Google Chrome 9_26_2023 12_00_43 PM.png";
 import moviebox from "./assets/images/React App - Google Chrome 9_26_2023 1_03_53 PM.png";
-import you from './assets/images/WhatsApp Image 2023-09-11 at 11.39.33.jpeg'
+import you from "./assets/images/WhatsApp Image 2023-09-11 at 11.39.33.jpeg";
 
 function App() {
   const [showSigninModal, setShowSigninModal] = useState(false);
@@ -90,145 +90,150 @@ function App() {
         Projects
       </p>
 
-      <div className="mx-10 border-t-2 border-[#bfa6ff2a]">
-        <div className="flex w-full my-10 flex-col md:flex-row gap-5">
-          <div className="w-full flex flex-col items-center md:items-start justify-center gap-2 pb-5">
-            <p className="text-[#8758FF70] text-xl font-bold my-2">HousePadi</p>
-            <p className="text-sm my-1 leading-7">
-              At HousePadi, we are redefining the meaning of home convinence. As
-              a dynamic startup, we currently offer expertly crafted meals from
-              professional chef and top-tier laundry services that bring
-              freshness right to your door. Soon, we will be expanding our
-              repertoire to imclude hassle-free grocery shopping and meticulous
-              residential cleaning services. With HousePadi, you can relax
-              knowing we have got home needs covered.
-            </p>
-            <div className="flex text-[10px] gap-3 text-[#8758ff]">
-              <p>#React</p>
-              <p>#Javascript</p>
-              <p>#Tailwind css</p>
-            </div>
-            <div className="my-2 flex gap-3">
-              <a
-                href="https://www.housepadi.ng/"
-                className="flex items-center text-xs text-[#8758ff] bg-[#8758ff3d] px-3 py-1 rounded-md shadow shadow-[#8758FF]"
-              >
-                Housepadi
-              </a>
-            </div>
-          </div>
-
-          <div className="w-full bg-[#070215] rounded-xl px-5">
-            <div className="flex justify-center relative top-4">
-              <p className="text-md lg:text-2xl xl:text-3xl font-semibold text-[#8758FF]">
-                Housepadi
+      <div className="flex flex-col gap-8">
+        <div className="mx-10 border-t-2 border-[#bfa6ff2a]">
+          <div className="flex w-full my-10 flex-col md:flex-row gap-5">
+            <div className="w-full flex flex-col items-center md:items-start justify-center gap-2 pb-5">
+              <p className="text-[#8758FF70] text-xl font-bold my-2">
+                HousePadi
               </p>
-            </div>
-            <img className="pt-10" src={housepadi} />
-          </div>
-        </div>
-      </div>
-
-      <div className=" mx-10">
-        <div className="flex w-full my-10 flex-col-reverse md:flex-row">
-          <div className="w-full bg-[#070215] rounded-xl px-5">
-            <div className="flex justify-center relative top-4">
-              <p className="text-md lg:text-2xl xl:text-3xl font-semibold text-[#8758ff3d]">
-                Lamore
+              <p className="text-sm my-1 leading-7">
+                At HousePadi, we are redefining the meaning of home convinence.
+                As a dynamic startup, we currently offer expertly crafted meals
+                from professional chef and top-tier laundry services that bring
+                freshness right to your door.
               </p>
-            </div>
-            <img className="pt-10" src={lamore} />
-          </div>
-
-          <div className="w-full flex flex-col items-center gap-2 md:items-start justify-center p-5">
-            <p className="text-[#8758FF70] text-xl font-bold">Lamore</p>
-            <p className="text-sm my-1 leading-7">
-              This is an event management web Platform. You can organize a wide
-              range of events and collaborate with various vendors who provide
-              services tailored to your various event requirements
-            </p>
-            <div className="flex text-[10px] gap-3 text-[#8758ff]">
-              <p>#React</p>
-              <p>#Javascript</p>
-              <p>#Tailwind css</p>
+              <div className="flex text-[10px] gap-3 text-[#8758ff]">
+                <p>#React</p>
+                <p>#Javascript</p>
+                <p>#Tailwind css</p>
+              </div>
+              <div className="my-2 flex gap-3">
+                <a
+                  href="https://www.housepadi.ng/"
+                  className="flex items-center text-xs text-[#8758ff] bg-[#8758ff3d] px-3 py-1 rounded-md shadow shadow-[#8758FF]"
+                >
+                  Housepadi
+                </a>
+              </div>
             </div>
 
-            <div className="my-2 flex gap-3">
-              <a
-                href="https://lamore-build.netlify.app/"
-                className="flex items-center text-xs text-[#8758ff] bg-[#8758ff3d] px-3 py-1 rounded-md shadow shadow-[#8758FF]"
-              >
-                Lamore
-              </a>
+            <div className="w-full bg-[#070215] rounded-xl px-5">
+              <div className="flex justify-center relative top-4">
+                <p className="text-md lg:text-2xl xl:text-3xl font-semibold text-[#8758ff3d]">
+                  Housepadi
+                </p>
+              </div>
+              <img className="pt-10" src={housepadi} />
             </div>
           </div>
         </div>
-      </div>
 
-      <div className=" mx-10">
-        <div className="flex w-full my-10 flex-col md:flex-row">
-          <div className="w-full flex flex-col gap-2 items-center md:items-start justify-center p-5 ">
-            <p className="text-[#8758FF70] text-xl font-bold">Comptware</p>
-            <p className="text-sm my-1 leading-7">
-              Empowering your business with cutting-edge technology
-            </p>
-            <div className="flex text-[10px] gap-3">
-              <p>#VS Code</p>
-              <p>#React</p>
-              <p>#Javascript</p>
-              <p>#Tailwind css</p>
+        <div className=" mx-10">
+          <div className="flex w-full my-10 flex-col-reverse md:flex-row">
+            <div className="w-full bg-[#070215] rounded-xl px-5">
+              <div className="flex justify-center relative top-4">
+                <p className="text-md lg:text-2xl xl:text-3xl font-semibold text-[#8758ff3d]">
+                  Lamore
+                </p>
+              </div>
+              <img className="pt-10" src={lamore} />
             </div>
 
-            <div className="my-2 flex gap-3">
-              <a
-                href="#"
-                className="flex items-center text-xs text-[#8758ff] bg-[#8758ff3d] px-3 py-1 rounded-md shadow shadow-[#8758FF]"
-              >
-                Comptware
-              </a>
-            </div>
-          </div>
-
-          <div className="w-full bg-[#070215] rounded-xl px-5">
-            <div className="flex justify-center relative top-4">
-              <p className="text-md lg:text-2xl xl:text-3xl font-semibold text-[#8758FF]">
-                Comptware
+            <div className="w-full flex flex-col items-center gap-2 md:items-start justify-center p-5">
+              <p className="text-[#8758FF70] text-xl font-bold">Lamore</p>
+              <p className="text-sm my-1 leading-7">
+                This is an event management web Platform. You can organize a
+                wide range of events and collaborate with various vendors who
+                provide services tailored to your various event requirements
               </p>
+              <div className="flex text-[10px] gap-3 text-[#8758ff]">
+                <p>#React</p>
+                <p>#Javascript</p>
+                <p>#Tailwind css</p>
+              </div>
+
+              <div className="my-2 flex gap-3">
+                <a
+                  href="https://lamore-build.netlify.app/"
+                  className="flex items-center text-xs text-[#8758ff] bg-[#8758ff3d] px-3 py-1 rounded-md shadow shadow-[#8758FF]"
+                >
+                  Lamore
+                </a>
+              </div>
             </div>
-            <img className="pt-10" src={comptware} />
           </div>
         </div>
-      </div>
 
-      <div className=" mx-10">
-        <div className="flex w-full my-10 flex-col-reverse md:flex-row">
-          <div className="w-full bg-[#070215] rounded-xl px-5">
-            <div className="flex justify-center relative top-4">
-              <p className="text-md lg:text-2xl xl:text-3xl font-semibold text-[#8758FF]">
-                MovieBox
+        <div className=" mx-10">
+          <div className="flex w-full my-10 flex-col md:flex-row">
+            <div className="w-full flex flex-col gap-2 items-center md:items-start justify-center p-5 ">
+              <p className="text-[#8758FF70] text-xl font-bold">Comptware</p>
+              <p className="text-sm my-1 leading-7">
+                Empowering your business with cutting-edge technology
               </p>
+              <div className="flex text-[10px] gap-3 text-[#8758ff]">
+                <p>#VS Code</p>
+                <p>#React</p>
+                <p>#Javascript</p>
+                <p>#Tailwind css</p>
+              </div>
+
+              <div className="p-1 shadow shadow-[#8758FF] rounded-[4px] mt-2">
+                <p className="text-sm text-[#8758ff]">In Progress...</p>
+              </div>
+
+              {/* <div className="my-2 flex gap-3">
+                <a
+                  href="#"
+                  className="flex items-center text-xs text-[#8758ff] bg-[#8758ff3d] px-3 py-1 rounded-md shadow shadow-[#8758FF]"
+                >
+                  Comptware
+                </a>
+              </div> */}
             </div>
-            <img className="pt-10" src={moviebox} />
+
+            <div className="w-full bg-[#070215] rounded-xl px-5">
+              <div className="flex justify-center relative top-4">
+                <p className="text-md lg:text-2xl xl:text-3xl font-semibold text-[#8758FF70]">
+                  Comptware
+                </p>
+              </div>
+              <img className="pt-10" src={comptware} />
+            </div>
           </div>
+        </div>
 
-          <div className="w-full flex flex-col items-center md:items-start justify-center p-5">
-            <p className="text-[#8758FF] text-xl font-bold">Moviebox</p>
-            <p className="text-sm my-1">
-              A project that displays a different movies based on genre.
-            </p>
-            <div className="flex text-[10px] gap-3">
-              <p>#React</p>
-              <p>#Javascript</p>
-              <p>#Tailwind css</p>
+        <div className=" mx-10">
+          <div className="flex w-full my-10 flex-col-reverse md:flex-row">
+            <div className="w-full bg-[#070215] rounded-xl px-5">
+              <div className="flex justify-center relative top-4">
+                <p className="text-md lg:text-2xl xl:text-3xl font-semibold text-[#8758FF70]">
+                  MovieBox
+                </p>
+              </div>
+              <img className="pt-10" src={moviebox} />
             </div>
 
-            <div className="my-2 flex gap-3">
-              <a
-                href="https://hng-stage-two.vercel.app/"
-                className="flex items-center text-xs text-[#8758ff] bg-[#8758ff3d] px-3 py-1 rounded-md border-2 border-[#8758FF]"
-              >
-                MovieBox
-              </a>
+            <div className="w-full flex flex-col gap-2 items-center md:items-start justify-center p-5">
+              <p className="text-[#8758FF70] text-xl font-bold">Moviebox</p>
+              <p className="text-sm my-1">
+                A project that displays a different movies based on genre.
+              </p>
+              <div className="flex text-[10px] gap-3 text-[#8758ff]">
+                <p>#React</p>
+                <p>#Javascript</p>
+                <p>#Tailwind css</p>
+              </div>
+
+              <div className="my-2 flex gap-3">
+                <a
+                  href="https://hng-stage-two.vercel.app/"
+                  className="flex items-center text-xs text-[#8758FF70] bg-[#8758ff3d] px-3 py-1 rounded-md shadow shadow-[#8758FF]"
+                >
+                  MovieBox
+                </a>
+              </div>
             </div>
           </div>
         </div>
@@ -244,14 +249,14 @@ function App() {
       <div className="px-10 text-xs flex items-center w-full gap-10 md:gap-0 py-14 flex-col-reverse md:flex-row">
         <div className="w-full flex flex-col gap-3">
           <p className="text-[#8758FF] font-black">Hi there. </p>
-          <p>
+          <p className="leading-5">
             My name is Ajiboye Ajibola, i am a frontend developer who has
-            acquired the working knowledge of HTML, CSS, Javascript, React, Typescript and Node
-            over the year through self-learning, to build fully responsive,
-            secure and functional web pages.
+            acquired the working knowledge of HTML, CSS, Javascript, React,
+            Typescript and Node over the year through self-learning, to build
+            fully responsive, secure and functional web pages.
           </p>
 
-          <div className="text-sm">
+          <div className="text-xs leading-6">
             <li>HTML</li>
             <li>CSS</li>
             <li>Javascript</li>
@@ -265,7 +270,10 @@ function App() {
 
         <div className="w-full flex justify-center">
           <div className="border-purple-400 border-2 h-96 w-[320px] sm:w-[340px] rounded-lg">
-            <img className="h-96 w-[330px] sm:w-[350px] ml-5 mt-5 rounded-lg" src={you} />
+            <img
+              className="h-96 w-[330px] sm:w-[350px] ml-5 mt-5 rounded-lg"
+              src={you}
+            />
           </div>
           {/* <img className='' src={image}/>  */}
         </div>
--- src/component/Footer/index.js
+++ src/component/Footer/index.js
@@ -9,7 +9,7 @@ import github from '../../assets/icons/github.png'
 const Footer = () => {
   return (
     <div className=' border-t-2 border-[#bfa6ff2a] mx-10 py-10 flex flex-col items-center'>
-        <p className='text-4xl font-semibold'>Get in touch</p>
+        <p className='text-2xl sm:text-3xl lg:text-4xl font-semibold'>Get in touch</p>
         <p className='text-xs my-4'>Hey, why don't you hit me up and let’s talk about your next project</p>
         <div className='flex gap-4'>
             <a href='http://ajibolaajiboye99@gmail.com'><img className='h-14' src={mail}/> </a>
